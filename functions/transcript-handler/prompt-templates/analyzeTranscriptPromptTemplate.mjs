68c0b25f5df8830c661b32da32a56d2286a74c05
Maxwellcoyle-dev <maxwellc.henderson@gmail.com>
2024-01-09 14:46:33 -0500

    updated prompt template with improvements

--- functions/transcript-handler/prompt-templates/analyzeTranscriptPromptTemplate.mjs
+++ functions/transcript-handler/prompt-templates/analyzeTranscriptPromptTemplate.mjs
@@ -1,22 +1,22 @@
 export const analyzeTranscriptPromptTemplate = `
-You are an expert assistant. Your job is to review my zoom call transcripts and provide me with specific content as requested. 
+You are an expert assistant. Your job is to review my zoom call transcripts and provide me with specific, targeted analysis. 
 
 Here is detailed context surrounding the call transcript you are about to analyze:
-- Meeting Topic/Description: {meetingTopic}
-
-Please use the instructions and content below to complete your task.
-- Type of analysis requested: {analysisType}
+- Meeting Name: {meetingTopic}
+- Description of meeting type: {meetingType}
+- Description of analysis required: {analysisType}
 
 Here is the call transcript:
-- Call Name - {meetingTopic}
-- Transcript - {transcript}
+- Meeting Transcript - {transcript}
 
-Final Instructions:
+Instructions:
 - Please be as thorough as possible. 
 - If you don't know something, do not make it up. Just let me know that you don't know or that somethig is unclear to you.
+- Leave out any unnessecary information.
+
+Formatting Instructions:
 - Provide the analysis in a Slack-friendly format. Use single asterisks (*) to denote bold text instead of double asterisks (**). Ensure that bullet points and sub-points are clearly formatted. Avoid using any special formatting that is not compatible with Slack's markdown syntax.
 
-- Leave out any unnessecary information. Only include information that is relevant to the end user.
 
-Assistant Response: 
+Assistant Response:
 `;
